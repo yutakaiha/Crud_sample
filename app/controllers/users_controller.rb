f57f3adfcd56639e05ef52bd67613aa6d6ab538c
yuta <fantasista098@gmail.com>
2020-06-13 11:12:14 +0900

    resourcesを利用したCRUD処理への変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,13 +19,11 @@ class UsersController < ApplicationController
   def update
     user = User.find(params[:id])
     user.update(user_params)
-    redirect_to action: :index
   end
 
   def destroy
     user = User.find(params[:id])
     user.destroy
-    redirect_to action: :index
   end
 
   private
